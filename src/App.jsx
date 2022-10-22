4b4ecb5cedb3cb24f08f03c8c4220562edaf6df9
abdullox0900 <abdullox0900@gmail.com>
2022-10-22 11:39:05 +0500

    feat: update in algoritim input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 import './App.scss';
 
 function App() {
@@ -7,91 +7,93 @@ function App() {
   const elBox = useRef()
   const elInput = useRef()
 
-  
+
 
   return (
     <>
-      <section className='certificate'>
-        <div className='certificate__top'>
-        </div>
-        <div className='certificate__body'>
-          <p>
-            Türkçe Öğretimi Uygulama ve Araştırma Merkezi (TÖMER)
-            Sürekli Eğitim Merkezi (SEM)
-          </p>
-          <h4>SERTİFİKA SORGULAMA</h4>
-        </div>
-
-        <form>
-        {/* Sertifika No */}
-          <input ref={elInput} type="text"  placeholder='Kayıt bulunamadı..' onChange={(evt) => setInValue(evt.target.value)}/>
-          <button type='button' onClick={() => {
-            if(inValue == '2210400152') {
-              elBox.current.classList.add('active');
-            } else {
-              setInValue('')
-              elInput.current.value = ''
-              // elInput.style.placeholder = "Kayit bulunamadi..";
-              elBox.current.classList.remove('active');
-            }
-          }}>GETİR</button>
-
-          <div className="sonuc" ref={elBox}>
-            <table>
-              <tbody>
-                <tr>
-                  <td>TC NO</td>
-                  <td>FA1380771</td>
-                </tr>
-                <tr>
-                  <td>ADI SOYADI</td>
-                  <td>ABDULLOX ABDUSALOMOV</td>
-                </tr>
-                <tr>
-                  <td>ÜLKESİ</td>
-                  <td>ÖZBEKİSTAN</td>
-                </tr>
-                <tr>
-                  <td>DİNLEME</td>
-                  <td>20</td>
-                </tr>
-                <tr>
-                  <td>OKUMA</td>
-                  <td>19,5</td>
-                </tr>
-                <tr>
-                  <td>YAZMA</td>
-                  <td>18</td>
-                </tr>
-                <tr>
-                  <td>KONUŞMA</td>
-                  <td>14</td>
-                </tr>
-                <tr>
-                  <td>TOPLAM</td>
-                  <td>71,5</td>
-                </tr>
-                <tr>
-                  <td>SONUÇ</td>
-                  <td>C1</td>
-                </tr>
-                <tr>
-                  <td>SINAV TARİHİ</td>
-                  <td>3 Ekim 2022</td>
-                </tr>
-                <tr>
-                  <td>BELGE TARİHİ</td>
-                  <td>4 Ekim 2022</td>
-                </tr>
-                <tr>
-                  <td>SERTİFİKA NO</td>
-                  <td>2210400152</td>
-                </tr>
-              </tbody>
-            </table>
+      <div className='container'>
+        <section className='certificate'>
+          <div className='certificate__top'>
           </div>
-        </form>
-      </section>
+          <div className='certificate__body'>
+            <p>
+              Türkçe Öğretimi Uygulama ve Araştırma Merkezi (TÖMER)
+              Sürekli Eğitim Merkezi (SEM)
+            </p>
+            <h4>SERTİFİKA SORGULAMA</h4>
+          </div>
+
+          <form>
+            {/* Sertifika No */}
+            <input ref={elInput} type="text" placeholder='Kayıt bulunamadı..' onChange={(evt) => setInValue(evt.target.value)} />
+            <button type='button' onClick={() => {
+              if (inValue == '2210400110') {
+                elBox.current.classList.add('active');
+              } else {
+                setInValue('')
+                elInput.current.value = ''
+                // elInput.style.placeholder = "Kayit bulunamadi..";
+                elBox.current.classList.remove('active');
+              }
+            }}>GETİR</button>
+
+            <div className="sonuc active" ref={elBox}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>TC NO</td>
+                    <td>FA1380771</td>
+                  </tr>
+                  <tr>
+                    <td>ADI SOYADI</td>
+                    <td>Olimjon Khaidarov</td>
+                  </tr>
+                  <tr>
+                    <td>ÜLKESİ</td>
+                    <td>ÖZBEKİSTAN</td>
+                  </tr>
+                  <tr>
+                    <td>DİNLEME</td>
+                    <td>20</td>
+                  </tr>
+                  <tr>
+                    <td>OKUMA</td>
+                    <td>19,5</td>
+                  </tr>
+                  <tr>
+                    <td>YAZMA</td>
+                    <td>18</td>
+                  </tr>
+                  <tr>
+                    <td>KONUŞMA</td>
+                    <td>14</td>
+                  </tr>
+                  <tr>
+                    <td>TOPLAM</td>
+                    <td>71,5</td>
+                  </tr>
+                  <tr>
+                    <td>SONUÇ</td>
+                    <td>C1</td>
+                  </tr>
+                  <tr>
+                    <td>SINAV TARİHİ</td>
+                    <td>3 Ekim 2022</td>
+                  </tr>
+                  <tr>
+                    <td>BELGE TARİHİ</td>
+                    <td>4 Ekim 2022</td>
+                  </tr>
+                  <tr>
+                    <td>SERTİFİKA NO</td>
+                    <td>2210400152</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </form>
+        </section>
+      </div>
     </>
   );
 }
--- src/Utils/inputV.js
+++ src/Utils/inputV.js
@@ -1,4 +1,4 @@
-function insertionSort (inputArr) {
+function insertionSort(inputArr) {
     let length = inputArr.length;
     for (let i = 1; i < length; i++) {
         let key = inputArr[i];
@@ -12,6 +12,49 @@ function insertionSort (inputArr) {
     return inputArr;
 };
 
-    let inputArr = [2,4,5,36,2,6,26,8,3,7]; // I want here user input which comes from html form instead of hard-coding elements of array like this
-    insertionSort(inputArr);
-    // console.log(inputArr);
+let inputArr = [2, 4, 5, 36, 2, 6, 26, 8, 3, 7]; // I want here user input which comes from html form instead of hard-coding elements of array like this
+insertionSort(inputArr);
+// console.log(inputArr);
+
+var A = [2, 1, 3, 4, 5];
+var B = [];
+var n = 5
+var m = 12
+big = -1;
+for (i = 0; i <= n; i++) {
+    sum = 0;
+    for (j = i; j <= n; j++) {
+        sum += A[j];
+        if (sum < m) { B[i] = sum; continue; }//B[i] становится равным сумме
+        if (sum > m) { B[i] = (sum - A[j]); break; }
+        if (sum == m) {
+            B[i] = sum //B[i] становится равным сумме 
+            break;
+        }
+    }
+    if (B[i] > big)
+        big = B[i];
+}
+
+/// variant Two
+
+// var A = [2, 1, 3, 4, 5];
+//     var B = [];
+//     var n = 5
+//     var m = 12
+//     big = -1;
+//     for (i = 0; i <= n; i++) {
+//         sum=0;
+//         for (j = i; j <= n; j++) {
+//             sum += A[j];
+//             if (sum < m) { B[i] = sum; continue; }//B[i] становится равным сумме
+//             if (sum > m) { B[i] = (sum - A[j]); break; }
+//             if (sum == m) {
+//               B[i] = sum //B[i] становится равным сумме 
+//                 break;
+//             }
+//         }
+//         if (B[i] > big)
+//             big = B[i];
+//     }
+//     alert(big);
